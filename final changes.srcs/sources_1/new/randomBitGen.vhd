dd9ad26c78e73b9c86a77893af607d8363a7bd86
Mert Ertuğrul <32410770+Ertugrulmert@users.noreply.github.com>
2020-06-09 01:16:20 +0200

    Update randomBitGen.vhd

--- final changes.srcs/sources_1/new/randomBitGen.vhd
+++ final changes.srcs/sources_1/new/randomBitGen.vhd
@@ -2,18 +2,9 @@
 --a pseudo-random number generator based on the internal clock of the FPGA
 ----------------------------------------------------------------------------------
 
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity randomBitGen is
     Port ( clock : in STD_LOGIC;
