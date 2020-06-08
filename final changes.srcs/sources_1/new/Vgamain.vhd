0e76bda8e0e6f911968a7816a1dea123eacd9c0f
Mert Ertuğrul <32410770+Ertugrulmert@users.noreply.github.com>
2020-06-09 01:15:59 +0200

    Update Vgamain.vhd

--- final changes.srcs/sources_1/new/Vgamain.vhd
+++ final changes.srcs/sources_1/new/Vgamain.vhd
@@ -1,4 +1,6 @@
-
+----------------------------------------------------------------------------------
+--combines other modules and directs signals to them
+----------------------------------------------------------------------------------
 
 
 library IEEE;
@@ -6,15 +8,6 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use work.Constants.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity Vgamain is
     Port ( topclock : in STD_LOGIC;
